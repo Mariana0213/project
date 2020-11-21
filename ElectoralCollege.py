61ee1d32cdb4a5d759af24b059cad4f278b7e5e6
Mariana0213 <63981079+Mariana0213@users.noreply.github.com>
2020-11-20 23:37:55 -0500

    Add files via upload

---
+++ ElectoralCollege.py
@@ -0,0 +1,24 @@
+import matplotlib.pyplot as plt
+def read_data(file_name):
+    data = open(file_name)
+    results = []
+    data.readline()
+    for row in data.readlines():
+        row = row.split(',')
+        results.append(tuple([row[0], int(row[1]), row[2].strip()]))
+    data.close()
+    return results
+
+
+
+
+
+
+if __name__ == '__main__':
+    results = read_data('../ElectorialCollege2/electoral_data.csv')
+    print(results)
+
+
+
+
+
